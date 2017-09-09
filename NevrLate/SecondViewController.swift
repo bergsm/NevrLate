15fdab343e6ef57f3bca5d6586ac30dea292d137
Shawn Berg <bergshawnm@gmail.com>
2017-09-09 11:42:34 -0500

    Initial Commit

---
+++ NevrLate/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NevrLate
+//
+//  Created by Shawn Berg on 9/9/17.
+//  Copyright © 2017 Shawn Berg. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
